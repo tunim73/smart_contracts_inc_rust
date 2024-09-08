874e649583f196c32e20e0189fa0ae32b61e7a67
tunim73 <antonio.netto973@gmail.com>
2024-09-08 19:14:23 -0300

    Save

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,12 @@
 // ink_metadata = { version = "5.0.0", default-features = false }
 #[ink::contract]
 mod knowledge_project {
-
     use ink::prelude::string::String;
+    use ink::storage::StorageVec;
 
-    #[ink::storage_item]
+    #[derive(Default)]
+    #[cfg_attr(feature = "std", derive(ink::storage::traits::StorageLayout))]
+    #[ink::scale_derive(Encode, Decode, TypeInfo)]
     pub struct Auction {
         name: String,
         status: bool,
@@ -21,23 +23,43 @@ mod knowledge_project {
 
     #[ink(storage)]
     pub struct MyContract {
-        // Store Auctions in a vec
         auctions: Auction,
-        items: Item
+        items: Item,
+        xuxas_string: StorageVec<String>
     }
 
     impl MyContract {
         #[ink(constructor)]
         pub fn new(name: String) -> Self {
+
+            let mut xuxas = StorageVec::new();
+
+            // Exemplo de como adicionar um item ao StorageVec
+
+            let a: String = "dada".parse().unwrap();
+            xuxas.push(&a);
+
+
             Self {
                 auctions: Auction {
                     name,
                     status: false,
                     finalized: false,
                 },
-                items: Item::Bye
+                items: Item::Bye,
+                xuxas_string:xuxas ,
             }
         }
+
+        #[ink(constructor)]
+        pub fn default() -> Self {
+            Self {
+                auctions: Default::default(),
+                items: Item::Oi,
+                xuxas_string: Default::default(),
+            }
+        }
+
 
         #[ink(message)]
         pub fn get(&self) -> (bool) {
